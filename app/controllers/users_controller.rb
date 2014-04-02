ca055f77df5f1334abd5b95975c09f1b397ad978
Mark Kreyman <mark@kreyman.com>
2014-04-01 19:40:11 -0600

    Fixed User controller and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,8 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password_digest, :omniauth)
+      params.require(:user).permit(
+          :first_name, :last_name, :email,
+          :password, :password_confirmation, :omniauth)
     end
 end
